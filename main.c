e1c518bc1d2211747846667d19b2a18e73fb3bec
Isaac Reyes <isaac.reyalves@gmail.com>
2024-05-09 20:28:10 -0300

    fix: read file function caused seg fault on carriage return chars imp: added a large input file

--- main.c
+++ main.c
@@ -51,26 +51,45 @@ int grava_arquivo(char *nome_arq, char *data) {
     return 1;
 }
 
-
 char * le_arquivo(char *nome_arq) {
+    char *data = NULL;
+    char *line = NULL;
+    int pa, pb, num_poks, i;
+    size_t str_size;
+    float atk, def, hp;
+    char nome[20], tipo[9];
+
     FILE *arq = fopen(nome_arq, "r");
     if (arq == NULL) return NULL;
 
-    fseek(arq, 0, SEEK_END);
-    long tam = ftell(arq);
-    rewind(arq);
+    fscanf(arq, " %d %d ", &pa, &pb);
+    num_poks = pa + pb;
 
-    char *data = malloc((size_t) tam + 1);
-    fread(data, 1, (size_t) tam, arq);
-    data[tam] = '\0';
-    fclose(arq);
+    str_size = 2 * sizeof(int) + 1;
+    data = realloc(data, str_size);
+    if (data == NULL) exit(1);
+    snprintf(data, str_size, "%d %d\n", pa, pb);
 
+    for (i = 0; i < num_poks; i++) {
+        fscanf(arq, " %s %f %f %f %s ", nome, &atk, &def, &hp, tipo);
+
+        str_size = strlen(data) + strlen(nome) + (3 * sizeof(float)) + strlen(tipo) + 2;
+        line = realloc(line, str_size);
+        if (line == NULL) exit(1);
+        snprintf(line, str_size, "%s %.1f %.1f %.1f %s\n", nome, atk, def, hp, tipo);
+        data = realloc(data, strlen(data) + strlen(line) + 1);
+        if (data == NULL) exit(100);
+        strcat(data, line);
+    }
+
+    printf("%s\n", data);
     return data;
 }
 
 
 char ** divide_linha(char * line) {
     char ** palavras = (char **) malloc(sizeof(char*) * 100);
+    if (palavras == NULL) exit(1);
     int indice = 0;
     char * inicio, *fim;
 
@@ -80,7 +99,8 @@ char ** divide_linha(char * line) {
         fim = inicio;
         while (*fim != ' ' && *fim != '\0') fim++;
 
-        palavras[indice] = malloc(fim - inicio + 1);
+        palavras[indice] = (char *) malloc(fim - inicio + 1);
+        if (palavras[indice] == NULL) exit(1);
         strncpy(palavras[indice], inicio, fim - inicio);
         palavras[indice][fim - inicio] = '\0';
 
@@ -120,7 +140,7 @@ TJogador * cria_jogadores(char *data) {
         else {
             char ** palavras = divide_linha(line);
             int player = i <= jogadores[0].num_poks ? 0 : 1;
-            j = j > jogadores[player].num_poks ? 0 : j;
+            j = j >= jogadores[player].num_poks ? 0 : j;
 
             jogadores[player].poks[j].nome = malloc(strlen(palavras[0]) + 1);
             jogadores[player].poks[j].tipo = malloc(strlen(palavras[4]) + 1);
@@ -142,7 +162,7 @@ TJogador * cria_jogadores(char *data) {
 
 
 int classifica_pokemon(const char *tipo) {
-    const char *tipos[] = {"gelo", "pedra", "eletrico", "agua"};
+    const char *tipos[] = {"fogo", "pedra", "eletrico", "agua"}; // TODO: add type 'gelo'
 
     for (int i = 0; i < 4; i++) {
         if (strcmp(tipo, tipos[i]) == 0)
@@ -267,6 +287,7 @@ int main(int argc, char *argv[]) {
     cria_log();
 
     if (argc == 2) {
+        printf("Usando arquivo %s\n", argv[1]);
         data = le_arquivo(argv[1]);
     } else {
         grava_arquivo(nome_arquivo, "[+]-----------------------------[+]\nNenhum arquivo informado.\nUsando o arquivo 'input.txt'.\nUso: main.exe <arquivo>.txt\n[+]-----------------------------[+]\n");

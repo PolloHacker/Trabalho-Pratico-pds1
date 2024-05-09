18862784c4868dc9a9f43d3a82e2f3c85f6dbdc2
Isaac Reyes <isaac.reyalves@gmail.com>
2024-05-09 11:14:44 -0300

    feature: added logs for each battle fix: corrected logic erros in attack function

--- main.c
+++ main.c
@@ -1,22 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 
 typedef struct pokemon {
     char *nome;
     char *tipo;
-    int hp;
-    int atk;
-    int def;
+    float hp;
+    float atk;
+    float def;
 
 } TPokemon;
 
 typedef struct jogador {
     int num_poks;
+    int current_pok;
     TPokemon *poks;
 
 } TJogador;
+
+char nome_arquivo[36], fstring[300];
+
+
+void cria_log() {
+    time_t now = time(0);
+    struct tm *t = localtime(&now);
+    strftime(nome_arquivo, sizeof(nome_arquivo), "./logs/log_%d-%m-%Y_%H-%M-%S.txt", t);
+}
+
+
+int verifica_arquivo(char *nome_arq) {
+    FILE *arq = fopen(nome_arq, "r");
+    if (arq == NULL) return 0;
+    fclose(arq);
+
+    return 1;
+}
+
+
+int grava_arquivo(char *nome_arq, char *data) {
+    char *modo = "w";
+    if (verifica_arquivo(nome_arq)) modo = "a";
+
+    FILE *arq = fopen(nome_arq, modo);
+    if (arq == NULL) return 0;
+    fprintf(arq, "%s\n", data);
+    fclose(arq);
+
+    return 1;
+}
+
+
+char * le_arquivo(char *nome_arq) {
+    FILE *arq = fopen(nome_arq, "r");
+    if (arq == NULL) return NULL;
+
+    fseek(arq, 0, SEEK_END);
+    long tam = ftell(arq);
+    rewind(arq);
+
+    char *data = malloc((size_t) tam + 1);
+    fread(data, 1, (size_t) tam, arq);
+    data[tam] = '\0';
+    fclose(arq);
+
+    return data;
+}
 
 
 char ** divide_linha(char * line) {
@@ -37,27 +87,11 @@ char ** divide_linha(char * line) {
         inicio = fim;
         indice++;
     }
-
     palavras[indice] = NULL;
 
     return palavras;
 }
 
-char * le_arquivo(char *nome_arq) {
-    FILE *arq = fopen(nome_arq, "r");
-    if (arq == NULL) return NULL;
-
-    fseek(arq, 0, SEEK_END);
-    long tam = ftell(arq);
-    rewind(arq);
-
-    char *data = malloc((size_t) tam + 1);
-    fread(data, 1, (size_t) tam, arq);
-    data[tam] = '\0';
-    fclose(arq);
-
-    return data;
-}
 
 TJogador * cria_jogadores(char *data) {
     TJogador * jogadores = (TJogador *) malloc(sizeof(TJogador) * 2);
@@ -66,8 +100,6 @@ TJogador * cria_jogadores(char *data) {
 
     token = strtok(data, "\n");
     while (token != NULL) {
-        printf("Novo token: %s\n", token);
-
         line = malloc(strlen(token) + 1);
         strcpy(line, token);
         line[strlen(token)] = '\0';
@@ -76,6 +108,7 @@ TJogador * cria_jogadores(char *data) {
             while (j < 2) {
                 if (line[i] != ' ') {
                     jogadores[j].num_poks = (int) line[i] - 48;
+                    jogadores[j].current_pok = 0;
                     jogadores[j].poks = (TPokemon *) malloc(sizeof(TPokemon) * jogadores[j].num_poks);
                     j++;
                 }
@@ -93,9 +126,9 @@ TJogador * cria_jogadores(char *data) {
             jogadores[player].poks[j].tipo = malloc(strlen(palavras[4]) + 1);
 
             strcpy(jogadores[player].poks[j].nome, palavras[0]);
-            jogadores[player].poks[j].atk =  atoi(palavras[3]);
-            jogadores[player].poks[j].def =  atoi(palavras[3]);
-            jogadores[player].poks[j].hp =  atoi(palavras[3]);
+            jogadores[player].poks[j].atk = (float) atoi(palavras[1]);
+            jogadores[player].poks[j].def = (float) atoi(palavras[2]);
+            jogadores[player].poks[j].hp = (float) atoi(palavras[3]);
             strcpy(jogadores[player].poks[j].tipo, palavras[4]);
             j++;
         }
@@ -105,7 +138,6 @@ TJogador * cria_jogadores(char *data) {
     }
 
     return jogadores;
-
 }
 
 
@@ -127,29 +159,121 @@ int mais_forte(char *tipo_atk, char * tipo_def) {
     return (atk + 1 == def || (atk == 4 && def == 0));
 }
 
+int mais_fraco(char *tipo_atk, char * tipo_def) {
 
-void ataca(TJogador *jogadores, int atacante) {
+    int atk = classifica_pokemon(tipo_atk);
+    int def = classifica_pokemon(tipo_def);
 
+    return (def + 1 == atk || (def == 4 && atk == 0));
 }
 
+void ataca(TJogador *jogadores, int atacante) {
+    int defensor = atacante == 0 ? 1 : 0;
+    float atk, def, hp;
+    char *forca = "";
+    int current_atk = jogadores[atacante].current_pok, current_def= jogadores[defensor].current_pok;
+
+    if (mais_forte(jogadores[atacante].poks[current_atk].tipo, jogadores[defensor].poks[current_def].tipo)) {
+        atk = (float) (jogadores[atacante].poks[current_atk].atk * 1.2);
+        forca = "forte";
+    }  
+    else if (mais_fraco(jogadores[atacante].poks[current_atk].tipo, jogadores[defensor].poks[current_def].tipo)) {
+        atk = (float) (jogadores[atacante].poks[current_atk].atk * 0.8);
+        forca = "fraco";
+    }
+    else {
+        atk = jogadores[atacante].poks[current_atk].atk;
+        forca = "normal";
+    }
+
+    def = jogadores[defensor].poks[current_def].def;
+    hp = jogadores[defensor].poks[current_def].hp;
+
+    snprintf(fstring, sizeof(fstring), "[+] Pokemon %s do jogador %d atacou pokemon %s do jogador %d com efeito de Atk %.1f/ Def %.1f. (%s)", jogadores[atacante].poks[current_atk].nome, atacante + 1, jogadores[defensor].poks[current_def].nome, defensor + 1, atk, def, forca);
+    grava_arquivo(nome_arquivo, fstring);
+
+    hp -= atk > def ? atk - def : 1;
+    snprintf(fstring, sizeof(fstring), "Pokemon %s agora tem %.1f de HP.\tInicial: %.1f\n", jogadores[defensor].poks[current_def].nome, hp, jogadores[defensor].poks[current_def].hp);
+    grava_arquivo(nome_arquivo, fstring);
+
+    jogadores[defensor].poks[current_def].hp = hp;
+
+    if (hp <= 0) {
+        jogadores[defensor].current_pok++;
+
+        snprintf(fstring, sizeof(fstring), "[!] Pokemon %s do jogador %d perdeu.\n\n[+]----------Fim de round----------[+]\n", jogadores[defensor].poks[current_def].nome, defensor + 1);
+        grava_arquivo(nome_arquivo, fstring);
+        printf("%s venceu %s\n", jogadores[atacante].poks[current_atk].nome, jogadores[defensor].poks[current_def].nome);
+    }
+}
+
+void checa_sobreviventes(TJogador *jogadores, int vencedor) {
+    int i;
+    grava_arquivo(nome_arquivo, "\n\n[!]----------Fim de jogo----------[!]\nChecando sobreviventes...\n");
+    printf("Pokemon sobreviventes:\n");
+    for (i = jogadores[vencedor].current_pok; i < jogadores[vencedor].num_poks; i++) {
+        snprintf(fstring, sizeof(fstring), "[+] Pokemon %s do jogador %d sobreviveu.\n", jogadores[vencedor].poks[i].nome, vencedor + 1);
+        grava_arquivo(nome_arquivo, fstring);
+        printf("%s\n", jogadores[vencedor].poks[i].nome);
+    }
+}
+
+void checa_derrotados(TJogador *jogadores) {
+    int i;
+    grava_arquivo(nome_arquivo, "\nChecando derrotados...\n");
+    printf("Pokemon derrotados:\n");
+    for (i = 0; i < jogadores[0].current_pok; i++) {
+        snprintf(fstring, sizeof(fstring), "[+] Pokemon %s do jogador 1 foi derrotado.\n", jogadores[0].poks[i].nome);
+        grava_arquivo(nome_arquivo, fstring);
+        printf("%s ", jogadores[0].poks[i].nome);
+    }
+    for (i = 0; i < jogadores[1].current_pok; i++) {
+        snprintf(fstring, sizeof(fstring), "[+] Pokemon %s do jogador 2 foi derrotado.\n", jogadores[1].poks[i].nome);
+        grava_arquivo(nome_arquivo, fstring);
+        printf("%s ", jogadores[1].poks[i].nome);
+    }
+    printf("\n");
+}
+
+
 void inicia_jogo(TJogador *jogadores) {
-    printf("%d\n", mais_forte(jogadores[0].poks[0].tipo, jogadores[1].poks[1].tipo));
+    grava_arquivo(nome_arquivo, "Jogo iniciado.");
+    int atacante = 0;
+    
+    while((jogadores[0].current_pok < jogadores[0].num_poks) && (jogadores[1].current_pok < jogadores[1].num_poks)) {
+        ataca(jogadores, atacante);
+        atacante = atacante == 0 ? 1 : 0;
+    }
+    if (jogadores[0].current_pok >= jogadores[0].num_poks) {
+        grava_arquivo(nome_arquivo, "Jogador 2 venceu.");
+        printf("Jogador 2 venceu.\n");
+    }
+    else {
+        grava_arquivo(nome_arquivo, "Jogador 1 venceu.");
+        printf("Jogador 1 venceu.\n");
+    }
+
+    checa_sobreviventes(jogadores, jogadores[0].current_pok < jogadores[0].num_poks ? 0 : 1);
+    checa_derrotados(jogadores);
+
+    system("pause");
 }
 
 
 int main(int argc, char *argv[]) {
-
     TJogador *jogadores;
     char *data;
     
+    cria_log();
+
     if (argc == 2) {
         data = le_arquivo(argv[1]);
     } else {
-        printf("[+]-----------------------------[+]\nNenhum arquivo informado.\nUsando o arquivo 'input.txt'.\nUso: main.exe <arquivo>.txt\n[+]-----------------------------[+]\n");
+        grava_arquivo(nome_arquivo, "[+]-----------------------------[+]\nNenhum arquivo informado.\nUsando o arquivo 'input.txt'.\nUso: main.exe <arquivo>.txt\n[+]-----------------------------[+]\n");
         data = le_arquivo("input.txt");
     }
     if (data == NULL) {
-        perror("Erro ao ler arquivo.");
+        grava_arquivo(nome_arquivo, "[!] Erro ao ler arquivo.");
         exit(1);
     }
 
@@ -157,9 +281,11 @@ int main(int argc, char *argv[]) {
     jogadores = cria_jogadores(data);
 
     for (int i = 0; i < 2; i++) {
-        printf("Jogador %d:\n", i + 1);
+        snprintf(fstring, sizeof(fstring), "Jogador %d:", i + 1);
+        grava_arquivo(nome_arquivo, fstring);
         for (int j = 0; j < jogadores[i].num_poks; j++) {
-            printf("- %s: %d/%d/%d %s\n", jogadores[i].poks[j].nome, jogadores[i].poks[j].atk, jogadores[i].poks[j].def, jogadores[i].poks[j].hp, jogadores[i].poks[j].tipo);
+            snprintf(fstring, sizeof(fstring), "- %s: Atk: %.1f\tDef: %.1f\tHp: %.1f\tTipo: %s\n", jogadores[i].poks[j].nome, jogadores[i].poks[j].atk, jogadores[i].poks[j].def, jogadores[i].poks[j].hp, jogadores[i].poks[j].tipo);
+            grava_arquivo(nome_arquivo, fstring);
         }
     }
 
